75217c19137d323c39ffb6db3bacddbb2ad901a5
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-08-22 11:32:54 -0300

    config: config datasource and docker-compose

---
+++ src/main/java/outercode/libraryAPI/config/DataSourceConfig.java
@@ -0,0 +1,55 @@
+package outercode.libraryAPI.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    //@Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }
+
+    @Bean
+    public DataSource hikariDatasource() {
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setPoolName("library-api-pool");
+        config.setMaxLifetime(600000); // 10 minutes
+        config.setConnectionTimeout(100000); // timeout for getting connection from pool
+        config.setConnectionTestQuery("select 1");
+        return new HikariDataSource(config);
+    }
+}
